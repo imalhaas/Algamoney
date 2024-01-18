9fdcc075dd20096dc7b692343b348e5a85faf2a5
Lucas <lucasdecassia9@hotmail.com>
2024-01-18 16:34:10 -0300

    Initial commit

---
+++ src/main/java/com/algaworksapi/algamoneyapi/AlgamoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworksapi.algamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+}
